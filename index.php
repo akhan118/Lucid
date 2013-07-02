407b46dbf61f1935cf3f9ae9b629828891bbd13f
akhan <akhan@192.168.1.139>
2013-07-01 22:40:08 -0400

    ast

---
+++ index.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+//die(phpinfo());
+
+/**
+ * Error reporting
+ */
+ini_set('display_errors', '1');
+error_reporting(E_ALL);
+
+
+
+
+/**
+ * Define Application root
+ */
+define('LUCID_ROOT', getcwd());
+
+/**
+ * Define Application core
+ * 
+ */
+
+
+ $core= getcwd().'/app/code/core/';
+
+ define('LUCID_CORE',$core );
+
+
+ /**
+ * Define Application Modules root
+ * 
+ */
+
+
+ $modules_root= getcwd().'/modules/';
+
+ define('LUCID_MODULES',$modules_root );
+
+ 
+ 
+ 
+ 
+ /**
+ * Define Application config files
+ * 
+ */
+
+
+$configFilename = LUCID_ROOT . '/app/config/config.php';
+
+/**
+ * include application configurations file
+ */
+
+require_once  $configFilename;
+
+
+
+//
+//if (!file_exists($configFilename)) {
+//    
+//        echo $configFilename." was not found";
+//   
+//    exit;
+//}
+
+
+?>
+
